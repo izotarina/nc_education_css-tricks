7e4b8922ecf2e3487da3afd0fec6ce9ab3696a8b
Irina Tarasova <izotarina@yandex.ru>
2021-06-10 08:34:31 +0300

    added media for big article

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,1 +1,8 @@
-console.log('Console works!')
+window.addEventListener(`resize`, event => {
+    let bigArticle = document.querySelector('#bigArticle');
+    if (window.innerWidth < 800) {
+        bigArticle.classList.remove('_big');
+    } else {
+        bigArticle.classList.add('_big');
+    }
+}, false);
