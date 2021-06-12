2044ed3ff7484deec3663d89781a0491099f2a86
Irina Tarasova <izotarina@yandex.ru>
2021-06-12 12:38:14 +0300

    added sticky position for sidebar

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,8 +1,21 @@
+let bigArticle = document.querySelector('#bigArticle');
+let sidebarBlock = document.querySelector('#sidebarBlock');
+let articlesAndSidebar = document.querySelector('#articlesAndSidebar');
+
 window.addEventListener(`resize`, event => {
-    let bigArticle = document.querySelector('#bigArticle');
     if (window.innerWidth < 800) {
         bigArticle.classList.remove('_big');
     } else {
         bigArticle.classList.add('_big');
+    }
+}, false);
+
+document.addEventListener('scroll', event => {
+    let topArticles = articlesAndSidebar.getBoundingClientRect().top;
+
+    if (topArticles < 0) {
+        sidebarBlock.classList.remove('_invisible');
+    } else {
+        sidebarBlock.classList.add('_invisible');
     }
 }, false);
